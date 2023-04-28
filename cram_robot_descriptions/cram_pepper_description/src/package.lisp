5c289e0f366987012ffcf12e83e6a58d79c8c29b
abdelker <abdelker@uni-bremen.de>
2023-04-28 11:49:44 +0200

    add pepper description

---
+++ cram_robot_descriptions/cram_pepper_description/src/package.lisp
@@ -0,0 +1,7 @@
+(in-package :cl-user)
+
+(defpackage cram-pepper-description
+  (:nicknames #:pepper-descr)
+  (:use #:common-lisp #:cram-prolog #:cram-robot-interfaces)
+  ;;(:export #:JULIETTEY20MP)
+  )
